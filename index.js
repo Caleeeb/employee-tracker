123a1d4ad4d5360ae11a12c704db684752bbbb12
Caleeeb <daycinematography@gmail.com>
2022-05-21 11:09:17 -0400

    forgot to promisify for database.js query setups

--- index.js
+++ index.js
@@ -2,3 +2,31 @@ const inquire = require('inquirer');
 const mysql = require('mysql2');
 const connection = require('./lib/connection');
 
+const { createEmployee, createRole, createDepartment, viewAllEmployees, viewAllRoles, viewAllDepartments } = require('')
+
+// IF I HAVE TIME: | bonus | view employee via manager or department, update employee, delete department or role or employee, view total salaries for each department
+
+// inquirer questions
+async function createEmployee() {
+
+}
+
+async function createRole() {
+
+}
+
+async function createDepartment() {
+
+}
+
+async function viewAllEmployees() {
+    const db = new Database(connection)
+}
+
+async function viewAllRoles() {
+
+}
+
+async function viewAllDepartments() {
+
+}
