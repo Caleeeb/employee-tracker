ae380cfbbe75195f343a5f120a989f09cca33e7c
Caleeeb <daycinematography@gmail.com>
2022-05-21 11:36:38 -0400

    main menu prompt added

--- index.js
+++ index.js
@@ -1,32 +1,61 @@
 const inquire = require('inquirer');
 const mysql = require('mysql2');
-const connection = require('./lib/connection');
+const Database = require('../lib/database');
+const connection = require('../lib/connection');
 
 const { createEmployee, createRole, createDepartment, viewAllEmployees, viewAllRoles, viewAllDepartments } = require('')
 
 // IF I HAVE TIME: | bonus | view employee via manager or department, update employee, delete department or role or employee, view total salaries for each department
 
+// array list for options for the mainMenuOptions
+
+let mainMenuOptionsList = [
+    "View all departments",
+    "View all roles",
+    "View all employees",
+    "Add a department",
+    "Add a role",
+    "Add a employee",
+    "Exit"
+]
+
+// display options like a main menu
+async function mainMenuOptions() {
+    const choice = await inquire.prompt([{
+        type: "list",
+        message: "What would you like to do?",
+        name: "choice",
+        choices: mainMenuOptionsList
+    }
+    ])
+        .then((answer) => {
+            answerPusher(answer);
+        });
+}
+
+
+
+
 // inquirer questions
-async function createEmployee() {
+// async function createEmployee() {
 
-}
+// }
 
-async function createRole() {
+// async function createRole() {
 
-}
+// }
 
-async function createDepartment() {
+// async function createDepartment() {
 
-}
+// }
 
-async function viewAllEmployees() {
-    const db = new Database(connection)
-}
+// async function viewAllEmployees() {
+// }
 
-async function viewAllRoles() {
+// async function viewAllRoles() {
 
-}
+// }
 
-async function viewAllDepartments() {
+// async function viewAllDepartments() {
 
-}
+// }
